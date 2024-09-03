0f8c5a6e8ac62b7896215986cdca25349730513f
francois-roseberry <f_roseberry@live.fr>
2024-09-03 11:19:58 -0400

    Add html linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,10 @@
-import html from "eslint-plugin-html"
+import htmlPlugin from "eslint-plugin-html"
 import js from "@eslint/js"
+import html from "@html-eslint/eslint-plugin"
+import htmlParser from "@html-eslint/parser"
 
 export default [{
+    ...html.configs["flat/recommended"],
     files: ["templates/**/*.html"],
     linterOptions: {
         reportUnusedDisableDirectives: "error"
@@ -13,12 +16,19 @@ export default [{
             navigator: "readonly",
             localStorage: "readonly",
         },
+        parser: htmlParser,
     },
     plugins: {
-        html,
+        htmlPlugin,
+        "@html-eslint": html,
     },
     settings: {
         "html/report-bad-indent": "error",
     },
-    rules: js.configs.recommended.rules,
+    rules: {
+        ...js.configs.recommended.rules,
+        ...html.configs["flat/recommended"].rules,
+        "@html-eslint/indent": "error",
+        "@html-eslint/require-lang": "off",
+    },
 }]
